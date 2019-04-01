ecbb91d191d8c984cda7beb7811fce37091759b9
Katarzyna Machura <machurakatarzyna@gmail.com>
2019-04-01 10:19:07 +0200

    added Controller for Main Pane

---
+++ Com/MrFever/Controller/MainController.java
@@ -0,0 +1,21 @@
+package Com.MrFever.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+
+public class MainController {
+
+    @FXML
+    private Pane mainPane;
+
+    @FXML
+    private Button childButton;
+
+    @FXML
+    private Button medButton;
+
+    @FXML
+    private Button exitButton;
+
+}
