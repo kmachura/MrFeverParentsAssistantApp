28760a6f8f5064a13b77b47441c69bdef3114883
Katarzyna Machura <machurakatarzyna@gmail.com>
2019-04-01 13:24:36 +0200

    added Child Profile Controller

---
+++ Com/MrFever/Controller/ChildProfileController.java
@@ -0,0 +1,17 @@
+package Com.MrFever.Controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Tab;
+
+public class ChildProfileController {
+
+    @FXML
+    private Tab childProfileTab;
+
+    @FXML
+    private Tab temperatureTab;
+
+    @FXML
+    private Tab medicinesTab;
+
+}
