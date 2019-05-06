f4344de513939b90051d6e1565ba60bac1366ce6
Katarzyna Machura <machurakatarzyna@gmail.com>
2019-05-06 17:17:18 +0200

    small changes in the Model of application

--- Com/MrFever/Model/Medicine.java
+++ Com/MrFever/Model/Medicine.java
@@ -1,29 +1,88 @@
 package Com.MrFever.Model;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
 public class Medicine {
-    private String substance;
-    private String type;
-
-    public String getSubstance() {
-        return substance;
+    private final SimpleStringProperty dateOfGiving;
+    private final SimpleStringProperty timeOfGiving;
+    private final SimpleStringProperty typeOfMedicine;
+    private final SimpleStringProperty formOfMedicine;
+    private final SimpleStringProperty doseOfMedicine;
+    
+    public Medicine(String dateOfGiving, String timeOfGiving, String typeOfMedicine, String formOfMedicine, String doseOfMedicine) {
+    	this.dateOfGiving = new SimpleStringProperty(dateOfGiving);
+    	this.timeOfGiving = new SimpleStringProperty(timeOfGiving);
+    	this.typeOfMedicine = new SimpleStringProperty(typeOfMedicine);
+    	this.formOfMedicine = new SimpleStringProperty(formOfMedicine);
+    	this.doseOfMedicine = new SimpleStringProperty(doseOfMedicine);
     }
 
-    public void setSubstance(String substance) {
-        this.substance = substance;
-    }
+	public String getDateOfGiving() {
+		return dateOfGiving.get();
+	}
 
-    public String getType() {
-        return type;
-    }
+	public void SetDateOfGiving(String value) {
+		dateOfGiving.setValue(value);
+	}
+	
+	public String getTimeOfGiving() {
+		return timeOfGiving.get();
+	}
+	
+	public void SetTimeOfGiving(String value) {
+		timeOfGiving.setValue(value);
+	}
 
-    public void setType(String type) {
-        this.type = type;
-    }
+	public String getTypeOfMedicine() {
+		return typeOfMedicine.get();
+	}
 
-    public Medicine() {}
+	public void SetTypeOfMedicine(String value) {
+		typeOfMedicine.setValue(value);
+	}
+	
+	public String getFormOfMedicine() {
+		return formOfMedicine.get();
+	}
+	
+	public void SetFormOfMedicine(String value) {
+		formOfMedicine.setValue(value);
+	}
 
-    public Medicine(String substance, String type){
-        this.setSubstance(substance);
-        this.setType(type);
-    }
+	public String getDoseOfMedicine() {
+		return doseOfMedicine.get();
+	}
+	
+	public void SetDoseOfMedicine(String value) {
+		doseOfMedicine.setValue(value);
+	}
+	
+	public StringProperty dateMProperty() {
+		return dateOfGiving;
+	}
+	
+	public StringProperty timeMProperty() {
+		return timeOfGiving;
+	}
+	
+	public StringProperty typeProperty() {
+		return typeOfMedicine;
+	}
+	
+	public StringProperty formProperty() {
+		return formOfMedicine;
+	}
+	
+	public StringProperty doseProperty() {
+		return doseOfMedicine;
+	}
+
+	@Override
+	public String toString() {
+		return dateOfGiving + " " + timeOfGiving + " " + typeOfMedicine + " " + formOfMedicine + " " + doseOfMedicine;
+	}
+	
+
+    
 }
